b333789c716fda934a679ed10e3f02b0c68fffeb
Joan Tolós López <joan@Joans-MBP-2.mm-ads.com>
2019-11-29 11:38:15 +0100

    Spring boot starter added

---
+++ src/main/java/com/joantolos/kata/scores/KataScoresApp.java
@@ -0,0 +1,12 @@
+package com.joantolos.kata.scores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataScoresApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataScoresApp.class, args);
+    }
+}
---
+++ src/main/java/com/joantolos/kata/scores/controller/ScoresController.java
@@ -0,0 +1,7 @@
+package com.joantolos.kata.scores.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ScoresController {
+}
